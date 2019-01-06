42daaa297ba429e27d89d0462faa66c9fe41b6d6
swarnalatapatra <swarnalata.sumi@gmail.com>
2019-01-06 03:15:01 +0100

    settings to run benchmark 131 cities

--- tsp/template/sw_cam/benchmark_plot.m
+++ tsp/template/sw_cam/benchmark_plot.m
@@ -5,12 +5,14 @@ function benchmark_plot(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_C
 
 [~ , ~, ~, best_per_gen] = run_experiment(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR_MUT, MUTATION, CROSSOVER, LOCALLOOP, ah1, ah2, ah3, STOP_CRIT,REPLACE_WORST,REPRESENTATION, number_of_runs);
 
+figure
 stdshade(best_per_gen,0.1,'r',0:(size(best_per_gen,2)-1));
 grid on
 xlabel('Generation');
 ylabel('Avg. Best solution per gen. across runs');  
 title1 =  sprintf('Benchmark problems with %d cities.' ,NVAR);
-title(title1);
+title2 = sprintf('Avg. results for %d independent runs.' ,number_of_runs);
+title({title1;title2});
 
 avg_best_sol = min(mean(best_per_gen_matx));
 
